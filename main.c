0baa03856cc189b8412905fe30763736ee1a6e8b
A-khateeb <khateebafeef@gmail.com>
2017-08-27 08:08:26 +0300

    Commit new changes

--- main.c
+++ main.c
@@ -1,44 +1,9 @@
 #include <stdio.h>
-#include <string.h>
-#define MAXLINE 1000
-int getthose(char line[], int lim);
-int strindex(char source[], char searchfor[]);
-char pattern[]="ould";
-
-main()
-{
-    char line[MAXLINE];
-    int found =0;
-    while (getthose(line, MAXLINE)>0)
-        if(strindex(line,pattern)>=0){
-            printf("%s",line);
-            found++;
-
-        }
-    return found;
-}
-
-int getthose(char s[],int lim)
-{
-    int c,i;
-    i=0;
-    while(--lim > 0 && (c=getchar()) !=EOF && c != '\n')
-        s[i++]=c;
-    if(c == '\n')
-        s[i++]=c;
-    s[i] ='\0';
-    return i;
-}
-
-int strindex(char s[],char t[]){
-    int i,j,k;
-    for(i=0;i<s[i]!='\0';i++){
-        for(j=0,k=0 ; t[k]!='\0' && s[j]==t[k];j++,k++)
-            ;
-        if(k>0 && t[k]=='\0')
-            return i;
-
-    }
-    return (-1);
+#include "Power.c"
+main(){
+    mains();
+    printf("Insert value");
+    getchar();
+    return 0;
 
 }
