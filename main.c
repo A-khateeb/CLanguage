f78cba8bd6e6b6cc884c116601c0fc79d25cc769
Afeef Khateeb <khateebafeef@gmail.com>
2017-10-14 23:54:40 +0300

    Commit new changes to the HEAD repo`

--- main.c
+++ main.c
@@ -1,17 +1,25 @@
-#include <stdlib.h>
 #include <stdio.h>
-
+#define ALLOCSIZE 1000
+static char allocbuf[ALLOCSIZE];
+static char *allocp = allocbuf;
 main()
 {
 
-    printf("Insert a value");
-    int x=1, y=2, z[10];
-    int *ip;
-    ip= &x;
-    y= *ip;
-    *ip = 0;
-    ip = &z[0];
-    printf(z,y,ip);
-    getchar();
     return 0;
+
+}
+
+char *alloc(int n)
+{
+    if(allocbuf+ALLOCSIZE - allocp >= n){
+        allocp +=n;
+        return allocp - n;
+
+    } else
+        return 0;
+}
+void afree(char *p)
+{
+    if(p>=allocbuf && p <allocbuf+ ALLOCSIZE)
+        allocp = p;
 }
